a50b2ff67a3daf213800c0e6dcec0c1fcbf84399
rehadhasan <rehadhasan664@gmail.com>
2024-11-06 20:51:59 +0600

    completed problem-3

--- problem-2.ts
+++ problem-2.ts
@@ -11,7 +11,5 @@ function removeDuplicates(array:number[]):number[] {
       
       return uniqueNumbers;
     }
-    
-
     const result = removeDuplicates([1, 2, 2, 3, 4, 4, 5,3,5,6]);
-    console.log(result); 
+    // console.log(result); 
---
+++ problem-3.ts
@@ -0,0 +1,20 @@
+// Write a TypeScript function countWordOccurrences that takes a sentence (string) and a word (string). It should return the number of times the word appears in the sentence, case-insensitively.
+
+{
+    function countWordOccurrences(sentence: string, word: string): number {
+    
+    const lowerCaseSentence = sentence.toLowerCase().split(' ');
+    const lowerCaseWord = word.toLowerCase();
+
+    let count = 0;
+    for (const word of lowerCaseSentence) {
+        if (word === lowerCaseWord) {
+            count++;
+        }
+    }
+    return count;
+}
+
+const result = countWordOccurrences("hello  world! Hello rehad. hello saiful", "hello");
+// console.log(result); 
+}
